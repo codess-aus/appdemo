4f77fd49b892f6ff0c4523708db6bd82f7476eef
Michelle Mei-Ling Sandford <codesswa@outlook.com>
2024-01-23 17:19:46 +0100

    Object Oriented Version

--- app.js
+++ app.js
@@ -1,69 +1,88 @@
-// db.js
+// Import the express module, which is a web application framework for Node.js
+const express = require('express');
+
+// Import the mongoose module, which is an Object Data Modeling (ODM) library for MongoDB and Node.js
 const mongoose = require('mongoose');
 
-const connectDB = (url) => mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-module.exports = connectDB;
-
-// user.js
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
-});
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
-
-// routes.js
-const express = require('express');
-const router = express.Router();
-
-const routes = (User) => {
-    router.get('/api/users', async (req, res) => {
-        const users = await User.find();
-        res.send(users);
-    });
-
-    router.post('/api/users', async (req, res) => {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            age: req.body.age
-        });
-        await user.save();
-        res.send(user);
-    });
-
-    return router;
-};
-
-module.exports = routes;
-
-// server.js
-const express = require('express');
-const connectDB = require('./db');
-const User = require('./user');
-const routes = require('./routes');
-
-const app = express();
-app.use(express.json());
-
-const startServer = async (port, dbUrl) => {
-    try {
-        await connectDB(dbUrl);
-        console.log('Connected to MongoDB');
-        app.use(routes(User));
-        app.listen(port, () => console.log(`Server listening on port ${port}`));
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
+class Database {
+    constructor(url) {
+        this.url = url;
     }
-};
+
+    connect() {
+        return mongoose.connect(this.url, { useNewUrlParser: true, useUnifiedTopology: true });
+    }
+}
+
+class User {
+    constructor() {
+        this.userSchema = new mongoose.Schema({
+            name: String,
+            email: String,
+            age: Number
+        });
+
+        this.UserModel = mongoose.model('User', this.userSchema);
+    }
+
+    async find() {
+        return this.UserModel.find();
+    }
+
+    async create(data) {
+        const user = new this.UserModel(data);
+        return user.save();
+    }
+}
+
+class Routes {
+    constructor(app, user) {
+        this.app = app;
+        this.user = user;
+    }
+
+    setupRoutes() {
+        this.app.get('/api/users', async (req, res) => {
+            const users = await this.user.find();
+            res.send(users);
+        });
+
+        this.app.post('/api/users', async (req, res) => {
+            const user = await this.user.create({
+                name: req.body.name,
+                email: req.body.email,
+                age: req.body.age
+            });
+            res.send(user);
+        });
+    }
+}
+
+class Server {
+    constructor(port, dbUrl) {
+        this.port = port;
+        this.dbUrl = dbUrl;
+        this.app = express();
+        this.app.use(express.json());
+        this.database = new Database(this.dbUrl);
+        this.user = new User();
+        this.routes = new Routes(this.app, this.user);
+    }
+
+    async start() {
+        try {
+            await this.database.connect();
+            console.log('Connected to MongoDB');
+            this.routes.setupRoutes();
+            this.app.listen(this.port, () => console.log(`Server listening on port ${port}`));
+        } catch (err) {
+            console.error('Failed to connect to MongoDB', err);
+        }
+    }
+}
 
 const port = process.env.PORT || 3000;
 const dbUrl = 'mongodb://localhost/mydatabase';
 
-startServer(port, dbUrl);
+const server = new Server(port, dbUrl);
+server.start();
